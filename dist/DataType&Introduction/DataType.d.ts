6024595481f79556fdf7b3b624b2c53493828f47
SussMan-Dev <duchuyendfield@gmail.com>
2026-05-16 22:29:16 +0700

    Remove dist folder from tracking

--- dist/DataType&Introduction/DataType.d.ts
+++
@@ -1,9 +0,0 @@
-declare let Name: string;
-declare let Age: number;
-declare let isMale: boolean;
-declare let course1: Array<string>;
-declare let course2: string[];
-declare let course3: {
-    name: string;
-};
-//# sourceMappingURL=DataType.d.ts.map
