4ca245a4f354a4326eae643f3ebacdf470e2146e
SussMan-Dev <duchuyendfield@gmail.com>
2026-05-18 17:20:41 +0700

    type aliases

---
+++ src/typeAliases/typeAliases.ts
@@ -0,0 +1,23 @@
+// Type aliases allow you to create custom types.
+
+// Type alias with union type
+type IsGay = "yes" | "no";
+
+let huy: IsGay = "yes"; // autocomplete supported
+
+console.log(huy); // "yes"
+console.log(typeof huy); // "string"
+
+// Type alias with object
+type Developer = {
+  tech: string;
+  experienceYears: number;
+};
+
+let ducHuy: Developer = {
+  tech: "TypeScript",
+  experienceYears: 1
+};
+
+console.log(ducHuy);
+console.log(typeof ducHuy); // "object"
