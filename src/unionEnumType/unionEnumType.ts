38cd1dc01c10d8171e000542aa1fd5e50dddd23d
SussMan-Dev <duchuyendfield@gmail.com>
2026-05-18 17:08:40 +0700

    added union type and enums course

--- src/unionEnumType/unionEnumType.ts
+++ src/unionEnumType/unionEnumType.ts
@@ -9,6 +9,13 @@ console.log(unionVariable);
 unionVariable = "hihi"
 console.log(unionVariable);
 
+//extra
+//union type is not only use for primitive, it can be use in type declaration, for example
+type isGirl = "yes"|"no"
+let isWritterGirl:isGirl = "no"
+console.log(isWritterGirl);
+
+
 // 2. Enum
 // Enum is a type that contains a list of predefined values.
 enum isHandSome{
@@ -18,3 +25,16 @@ enum isHandSome{
 let DucHuy :isHandSome = isHandSome.handSome
 console.log(typeof(DucHuy));
 console.log(DucHuy);
+
+// Union types and enums can both limit values,
+// so why do we need to learn enums if union types already exist?
+
+// Union types are easy to use when the number of values is small.
+// But when the list gets bigger, writing everything in one line becomes hard to manage.
+// That's one reason enums are useful.
+
+// Another important difference is that union types disappear after compilation
+// because JavaScript does not understand TypeScript types.
+
+// Enums, however, are compiled into real JavaScript objects
+// that still exist at runtime.
